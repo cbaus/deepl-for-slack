767517846eaf55065fddf6a92d7ef9b7b097f71e
cbaus <github_c@maat-family.de>
2026-03-19 07:08:40 -0400

    replicate update translate function in auto-translate

--- src/deepl.ts
+++ src/deepl.ts
@@ -162,22 +162,99 @@ export class DeepLApi {
 
   async autoTranslate(text: string): Promise<string | null> {
     let [sourceLang, targetLang] = this.guessSourceTargetLang(text);
+    return this.translate(text, targetLang)
     return this.axiosInstance({
+        this.axiosInstance({
+      method: "POST",
       url: "/translate",
-      data: qs.stringify({
-        auth_key: this.authKey,
-        text: text,
-        target_lang: targetLang
-      }),
+      data: {
+        text: [
+          // Before sending the text to the DeepL API,
+          // replace special syntax parts with ignore tags to keep them
+          text
+            .replace(/<(.*?)>/g, (_: any, match: string) => {
+              // match #channels and @mentions
+              if (match.match(/^[#@].*$/)) {
+                const matched = match.match(/^([#@].*)$/);
+                if (matched != null) {
+                  return "<mrkdwn>" + matched[1] + "</mrkdwn>";
+                }
+                return "";
+              }
+              // match subteam
+              if (match.match(/^!subteam.*$/)) {
+                return "@[subteam mention removed]";
+              }
+              // match date formatting
+              if (match.match(/^!date.*$/)) {
+                const matched = match.match(/^(!date.*)$/);
+                if (matched != null) {
+                  return "<mrkdwn>" + matched[1] + "</mrkdwn>";
+                }
+                return "";
+              }
+              // match special mention
+              if (match.match(/^!.*$/)) {
+                const matched = match.match(/^!(.*?)(?:\|.*)?$/);
+                if (matched != null) {
+                  return "<ignore>@" + matched[1] + "</ignore>";
+                }
+                return "<ignore>@[special mention]</ignore>";
+              }
+              // match formatted link
+              if (match.match(/^.*?\|.*$/)) {
+                const matched = match.match(/^(.*?)\|(.*)$/);
+                if (matched != null) {
+                  return '<a href="' + matched[1] + '">' + matched[2] + "</a>";
+                }
+                return "";
+              }
+              // fallback (raw link or unforeseen formatting)
+              return "<mrkdwn>" + match + "</mrkdwn>";
+              // match emoji
+            })
+            .replace(/:([a-z0-9_-]+):/g, (_, match: string) => {
+              return "<emoji>" + match + "</emoji>";
+            })
+        ],
+        target_lang: targetLanguage.toUpperCase(),
+        tag_handling: "xml",
+        ignore_tags: ["emoji", "mrkdwn", "ignore"],
+      },
       headers: {
-        "content-type": "application/x-www-form-urlencoded;charset=utf-8"
-      }
+        "Content-Type": "application/json",
+        "Authorization": `DeepL-Auth-Key ${this.authKey}`,
+      },
     }).then(response => {
       this.logger.debug(response.data);
       if (response.data.translations && response.data.translations.length > 0) {
         if (response.data.translations[0].detected_source_language === sourceLang) {
           this.logger.debug(`Guessed right, text was in ${response.data.translations[0].detected_source_language}`);
-          return response.data.translations[0].text;
+          return response.data.translations[0].text.replace(
+              /<emoji>([a-z0-9_-]+)<\/emoji>/g,
+              (_: any, match: string) => {
+                return ":" + match + ":";
+                // match <mrkdwn>...</mrkdwn>
+              }
+            )
+            .replace(/<mrkdwn>(.*?)<\/mrkdwn>/g, (_: any, match: string) => {
+              return "<" + match + ">";
+              // match <a href="...">...</a>
+            })
+            .replace(
+              /(<a href="(?:.*?)">(?:.*?)<\/a>)/g,
+              (_: any, match: string) => {
+                const matched = match.match(/<a href="(.*?)">(.*?)<\/a>/);
+                if (matched != null) {
+                  return "<" + matched[1] + "|" + matched[2] + ">";
+                }
+                return "";
+                // match <ignore>...</ignore>
+              }
+            )
+            .replace(/<ignore>(.*?)<\/ignore>/g, (_: any, match: string) => {
+              return match;
+            });
         } else {
           this.logger.debug(`Guessed wrong, text was ${response.data.translations[0].detected_source_language} now translating to ${sourceLang}`);
           return this.translate(text, sourceLang);
